e1f97f451a67c696cf052f52dd18acbdc586f8cc
ausrazali <109255635+ausrazali@users.noreply.github.com>
2024-01-29 09:41:25 +0800

    Initial commit

---
+++ src/Playground.java
@@ -0,0 +1,10 @@
+public class Playground {
+    public static void main(String[] args) {
+        String subjectName = "Object-Oriented Programming";
+        String taskName = "Assignment 2";
+        String groupName = "Group 3";
+
+        System.out.println("Hello, my fellow " + groupName + " members for " + taskName + ", " + subjectName + " course.");
+        System.out.println("Add your name below:\n1. Daus\n2. \n3. \n4. \n");
+    }
+}
